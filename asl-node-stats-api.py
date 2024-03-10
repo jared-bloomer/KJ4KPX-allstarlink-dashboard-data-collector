51f0dc488eed92607f60bf4afe084709280e600d
jared-bloomer <me@jaredbloomer.com>
2024-03-10 03:05:40 +0000

    Collecting more metrics

--- asl-node-stats-api.py
+++ asl-node-stats-api.py
@@ -34,10 +34,17 @@ def main():
     totalKeyUps = Point("totalKeyUps").tag("totalkeyups", stats['stats']['data']['totalkeyups']).field("totalKeyUps", int(stats['stats']['data']['totalkeyups'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
     totalTXTime = Point("totalTXTime").tag("totalTXTime", stats['stats']['data']['totaltxtime']).field("totalTXTime", int(stats['stats']['data']['totaltxtime'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
     totalKerchunks = Point("totalKerchunks").tag("totalKerchunks", stats['stats']['data']['totalkerchunks']).field("totalKerchunks", int(stats['stats']['data']['totalkerchunks'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
+    totalexecdcommands = Point("totalexecdcommands").tag("totalexecdcommands", stats['stats']['data']['totalexecdcommands']).field("totalexecdcommands", int(stats['stats']['data']['totalexecdcommands'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
+    keytime = Point("keytime").tag("keytime", stats['stats']['data']['keytime']).field("keytime", int(stats['stats']['data']['keytime'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
+    apprptuptime = Point("apprptuptime").tag("apprptuptime", stats['stats']['data']['apprptuptime']).field("apprptuptime", int(stats['stats']['data']['apprptuptime'])).time(datetime.now(tz=timezone.utc), WritePrecision.MS)
+
 
     write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=totalKeyUps)
     write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=totalTXTime)
     write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=totalKerchunks)
+    write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=totalexecdcommands)
+    write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=keytime)
+    write_api.write(bucket=""+config['influxdb']['Bucket']+"", record=apprptuptime)
 
 if __name__ == "__main__":
   try:
